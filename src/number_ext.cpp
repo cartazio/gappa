97ce4390ef40336f0ee1468aa41a6fd673deed98
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2004-07-11 23:34:02 +0000

    gestion des denormalises en entree

--- src/number_ext.cpp
+++ src/number_ext.cpp
@@ -2,10 +2,7 @@
 #include "number_ext.hpp"
 #include "ast.hpp"
 
-static int const prec[] = { real_prec, 24, 53, 64, 113 };
-
-impl_data *read_number(ast_number const &n, type_id type, mp_rnd_t rnd) {
-  int p = prec[type];
+static impl_data *read_number_data(ast_number const &n, int p, mp_rnd_t rnd) {
   impl_data *res = new impl_data;
   mpfr_set_prec(res->val, p);
   if (n.base == 10) {
@@ -20,4 +17,22 @@ impl_data *read_number(ast_number const &n, type_id type, mp_rnd_t rnd) {
     mpfr_set_ui(res->val, 0, rnd);
   }
   return res;
+}
+
+static int const prec[] = { real_prec, 24, 53, 64, 113 };
+static int const min_exp[] = { mpfr_get_emin(), -126, -1022, -16382, -16382 };
+
+impl_data *read_number(ast_number const &n, type_id type, mp_rnd_t rnd) {
+  if (n.base == 0) return read_number_data(n, real_prec, rnd);
+  impl_data *d;
+  int p = prec[type];
+  int emin = min_exp[type] - p + 1; // 2^emin == smallest positive floating point number (subnormal)
+  for(;;) {
+    d = read_number_data(n, p, rnd);
+    if (mpfr_sgn(d->val) == 0) return d;
+    int e = (d->val[0]._mpfr_exp - 1) - p + 1; // exponent of the least significative bit
+    if (e >= emin) return d;
+    delete d;
+    p += e - emin;
+  }
 }
