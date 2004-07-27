0ab43340f763e552f590adeaa201b1e7dad3cf6d
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2004-07-27 15:30:07 +0000

    extension de la dichotomie aux erreurs

--- src/dichotomy.cpp
+++ src/dichotomy.cpp
@@ -18,6 +18,7 @@ namespace dichotomy {
 
 namespace basic_proof {
 interval compute_bound(property_vect const &hyp, variable *v);
+node *generate_error(property_vect const &hyp, property &res);
 } // namespace basic_proof
 
 namespace {
@@ -35,10 +36,19 @@ struct dichotomy_failure {
   dichotomy_failure(property_vect const &h, property const &r, interval const &b): hyp(h), res(r), bnd(b) {}
 };
 
+interval compute_error(property_vect const &hyp, property const &res) { // TODO
+  property tmp = res;
+  tmp.bnd = interval();
+  graph_layer layer;
+  node *n = basic_proof::generate_error(hyp, tmp);
+  if (!n) return interval();
+  return tmp.bnd;
+}
+
 void dichotomize(property_vect &hyp, property &res, int idx) {
   property &h = hyp[idx];
-  assert(res.type == PROP_BND && h.type == PROP_BND);
-  interval bnd = basic_proof::compute_bound(hyp, res.var);
+  assert(h.type == PROP_BND);
+  interval bnd = res.type == PROP_BND ? basic_proof::compute_bound(hyp, res.var) : compute_error(hyp, res);
   if (is_defined(bnd) && bnd <= res.bnd) {
     //std::cout << "  " << h.bnd << " -> " << bnd << std::endl;
     res.bnd = bnd;
@@ -58,26 +68,33 @@ void dichotomize(property_vect &hyp, property &res, int idx) {
 } // anonymous namespace
 
 node *generate_proof(property_vect const &hyp, property const &res) {
-  if (node *n = generate_basic_proof(hyp, res)) return n;
-  if (res.type != PROP_BND || !is_defined(res.bnd)) return NULL;
+  {
+    graph_layer layer;
+    if (node *n = generate_basic_proof(hyp, res)) { layer.flatten(); return n; }
+  }
+  if (!is_defined(res.bnd)) return NULL;
   std::vector< variable * > vars = multiple_definition(res.var);
   int i;
-  property bnd(PROP_BND);
+  property bnd(PROP_BND); // TODO
   bnd.var = vars[0];
   i = hyp.find_compatible_property(bnd);
   assert(i >= 0);
-  bnd.var = res.var;
-  bnd.bnd = res.bnd;
+  property res2 = res;
   property_vect hyp2 = hyp;
   try {
-    dichotomize(hyp2, bnd, i);
+    dichotomize(hyp2, res2, i);
   } catch (dichotomy_failure e) {
-    std::cerr
-      << "dichotomy failure: when "
-      << e.hyp[i].var->name->name << " is " << e.hyp[i].bnd << ", "
-      << e.res.var->name->name << " is in " << e.bnd
-      << " potentially outside of " << e.res.bnd << std::endl;
+    property &h = e.hyp[i];
+    std::cerr << "dichotomy failure: when " << h.var->name->name << " is " << h.bnd << ", ";
+    property &p = e.res;
+    std::string const &name = p.var->name->name;
+    if (p.type == PROP_BND)
+      std::cerr << name;
+    else if (p.type == PROP_ABS || p.type == PROP_REL)
+      std::cerr << (p.type == PROP_ABS ? "ABS(" : "REL(") << name << ", ...)";
+    else assert(false);
+    std::cerr << " is in " << e.bnd << " potentially outside of " << p.bnd << std::endl;
     return NULL;
   }
-  return new node_plouf(hyp, bnd);
+  return new node_plouf(hyp, res2);
 }
