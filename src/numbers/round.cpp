1019617bd36026d5e99c9d13e40fc4c7af4a53e8
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-01-03 15:47:13 +0000

    Ajout des fonctions d'arrondis.

--- src/numbers/round.cpp
+++ src/numbers/round.cpp
@@ -1,4 +1,6 @@
 #include "round.hpp"
+#include <algorithm>
+#include <cassert>
 
 bool split_exact(mpfr_t const &f, mpz_t &frac, int &exp, bool &sign) { // return true if zero
   int sgn = mpfr_sgn(f);
@@ -10,4 +12,55 @@ bool split_exact(mpfr_t const &f, mpz_t &frac, int &exp, bool &sign) { // return
   if (d > 0) mpz_fdiv_q_2exp(frac, frac, d);
   exp += d;
   return false;
+}
+
+void rnd::shr(int d) {
+  assert(d >= 0);
+  if (d == 0) return;
+  e += d;
+  if (!(g || s || d == 1))
+    g = mpz_divisible_2exp_p(m, d - 1);
+  else s |= g;
+  g = mpz_tstbit(m, d - 1);
+  mpz_tdiv_q_2exp(m, m, d);
+}
+
+bool float_format::rndU(rnd const &r) const {
+  return r.g || r.s;
+}
+
+bool float_format::rndCE(rnd const &r) const {
+  return r.g && (r.s || !mpz_tstbit(r.m, 0));
+}
+
+void float_format::succ(mpz_t &m, int &e) const {
+  assert(mpz_sgn(m) >= 0 && mpz_sizeinbase(m, 2) <= prec && e >= min_exp);
+  if (e > min_exp)
+    mpz_mul_2exp(m, m, prec - mpz_sizeinbase(m, 2));
+  mpz_add_ui(m, m, 1);
+  if (mpz_sizeinbase(m, 2) > prec) {
+    mpz_set_ui(m, 1);
+    e += prec;
+  }
+}
+
+void float_format::trunc(mpfr_t const &f, rnd &r, bool &sign) const {
+  split_exact(f, r.m, r.e, sign);
+  r.s = r.g = false;
+  int size = mpz_sizeinbase(r.m, 2);
+  int dec = std::max(min_exp - r.e, size - (int)prec);
+  if (dec <= 0) return;
+  r.shr(dec);
+}
+
+void float_format::round(mpfr_t &f, rnd_fun g1, rnd_fun g2) const {
+  rnd r;
+  bool s;
+  trunc(f, r, s);
+  if ((this->*(s ? g2 : g1))(r)) succ(r.m, r.e);
+  mpfr_set_prec(f, prec);
+  int v = mpfr_set_z(f, r.m, GMP_RNDN);
+  assert(v == 0);
+  mpfr_mul_2si(f, f, r.e, GMP_RNDN);
+  if (s) mpfr_neg(f, f, GMP_RNDN);
 }
--- src/numbers/round.hpp
+++ src/numbers/round.hpp
@@ -6,4 +6,31 @@
 
 bool split_exact(mpfr_t const &f, mpz_t &frac, int &exp, bool &sign); // return true if zero
 
+struct rnd {
+  mpz_t m;
+  int e;
+  bool g, s;
+  rnd() { mpz_init(m); }
+  ~rnd() { mpz_clear(m); }
+  void shr(int d);
+};
+
+class float_format {
+  void succ(mpz_t &m, int &e) const;
+  void trunc(mpfr_t const &f, rnd &r, bool &sign) const;
+  typedef bool (float_format::*rnd_fun)(rnd const &) const;
+  void round(mpfr_t &f, rnd_fun g1, rnd_fun g2) const;
+  bool rndZ(rnd const &) const { return false; }
+  bool rndU(rnd const &) const;
+  bool rndCE(rnd const &) const;
+  float_format(unsigned p, int e): min_exp(e), prec(p) {}
+ public:
+  int min_exp;
+  unsigned prec;
+  void roundZ(mpfr_t &f) { round(f, &float_format::rndZ, &float_format::rndZ); }
+  void roundU(mpfr_t &f) { round(f, &float_format::rndU, &float_format::rndZ); }
+  void roundD(mpfr_t &f) { round(f, &float_format::rndZ, &float_format::rndU); }
+  void roundCE(mpfr_t &f) { round(f, &float_format::rndCE, &float_format::rndCE); }
+};
+
 #endif // ROUND_HPP
