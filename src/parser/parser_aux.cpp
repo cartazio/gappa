1d934ef2fca7d66fc3595485036acd185c23807f
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-01-10 19:08:40 +0000

    Parcours dans le bon sens.

--- src/parser/parser_aux.cpp
+++ src/parser/parser_aux.cpp
@@ -60,7 +60,7 @@ property generate_property(ast_atom_bound const &p, bool goal) {
 void generate_subgraph(ast_prop_impl const &p, node_id type) {
   ast_prop_and tmp = merge_prop_and(p.left);
   property_vect hyp;
-  for(int i = tmp.size() - 1; i >= 0; i--) {
+  for(int i = 0, l = tmp.size(); i < l; ++i) {
     ast_prop &q = tmp[i];
     if (ast_atom_bound *r = boost::get< ast_atom_bound >(&q))
       hyp.push_back(generate_property(*r, false));
@@ -71,7 +71,7 @@ void generate_subgraph(ast_prop_impl const &p, node_id type) {
     }
   }
   tmp = merge_prop_and(p.right);
-  for(int i = tmp.size() - 1; i >= 0; i--) {
+  for(int i = 0, l = tmp.size(); i < l; ++i) {
     ast_prop &q = tmp[i];
     node *n = new node(type);
     n->hyp = hyp;
@@ -85,7 +85,7 @@ void generate_graph(ast_prop const &p) {
   if (ast_prop_impl const *q = boost::get< ast_prop_impl >(&p)) {
     generate_subgraph(*q, CONCLUSION);
   } else if (ast_prop_and const *q = boost::get< ast_prop_and >(&p)) {
-    for(int i = q->size() - 1; i >= 0; i--)
+    for(int i = 0, l = q->size(); i < l; ++i)
       generate_graph((*q)[i]);
   } else {
     ast_prop_impl tmp;
