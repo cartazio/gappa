ffa6e89ab4b34f6dfe80d80a6b0d2963a3c5a505
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-01-15 02:19:48 +0000

    Previens pour les indefinis places n'importe ou. Et un peu de statictiques...

--- src/parser/parser_aux.cpp
+++ src/parser/parser_aux.cpp
@@ -38,7 +38,7 @@ ast_prop_and merge_prop_and(ast_prop const &_p1, ast_prop const &_p2) {
   return p;
 }
 
-ast_prop_and merge_prop_and(ast_prop const &_p) {
+static ast_prop_and merge_prop_and(ast_prop const &_p) {
   if (ast_prop_and const *p = boost::get< ast_prop_and const >(&_p))
     return *p;
   else {
@@ -48,38 +48,42 @@ ast_prop_and merge_prop_and(ast_prop const &_p) {
   }
 }
 
-property generate_property(ast_atom_bound const &p, bool goal) {
+static property generate_property(ast_atom_bound const &p, bool goal, bool axiom) {
   property r(p.real);
   if (p.interval.lower) {
     assert(p.interval.upper);
     r.bnd = create_interval(p.interval, goal);
-  } else assert(!p.interval.upper);
+  } else {
+    if (!goal || axiom)
+      { std::cerr << "Error: undefined intervals are reserved for conclusions.\n"; exit(1); }
+    assert(!p.interval.upper);
+  }
   return r;
 }
 
-void generate_axiom(ast_prop_impl const &p, node_vect &axioms) {
+static void generate_axiom(ast_prop_impl const &p, node_vect &axioms) {
   ast_prop_and tmp = merge_prop_and(p.left);
   property_vect hyp;
   for(int i = 0, l = tmp.size(); i < l; ++i) {
     ast_prop &q = tmp[i];
     if (ast_atom_bound *r = boost::get< ast_atom_bound >(&q))
-      hyp.push_back(generate_property(*r, false));
+      hyp.push_back(generate_property(*r, false, true));
     else { std::cerr << "Error: too complex a logical proposition.\n"; exit(1); }
   }
   tmp = merge_prop_and(p.right);
   for(int i = 0, l = tmp.size(); i < l; ++i) {
     if (ast_atom_bound *r = boost::get< ast_atom_bound >(&tmp[i]))
-      axioms.push_back(new axiom_node(hyp, generate_property(*r, true)));
+      axioms.push_back(new axiom_node(hyp, generate_property(*r, true, true)));
     else { std::cerr << "Error: too complex a logical proposition.\n"; exit(1); }
   }
 }
 
-void generate_subgraph(ast_prop_impl const &p, node_vect &axioms, property_vect &hyp, property_vect &goal) {
+static void generate_subgraph(ast_prop_impl const &p, node_vect &axioms, property_vect &hyp, property_vect &goal) {
   ast_prop_and tmp = merge_prop_and(p.left);
   for(int i = 0, l = tmp.size(); i < l; ++i) {
     ast_prop &q = tmp[i];
     if (ast_atom_bound *r = boost::get< ast_atom_bound >(&q))
-      hyp.push_back(generate_property(*r, false));
+      hyp.push_back(generate_property(*r, false, false));
     else {
       ast_prop_impl *r = boost::get< ast_prop_impl >(&q);
       assert(r);
@@ -89,7 +93,7 @@ void generate_subgraph(ast_prop_impl const &p, node_vect &axioms, property_vect 
   tmp = merge_prop_and(p.right);
   for(int i = 0, l = tmp.size(); i < l; ++i) {
     if (ast_atom_bound *r = boost::get< ast_atom_bound >(&tmp[i]))
-      goal.push_back(generate_property(*r, true));
+      goal.push_back(generate_property(*r, true, false));
     else { std::cerr << "Error: too complex a logical proposition.\n"; exit(1); }
   }
 }
