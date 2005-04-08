1c70b0c7094e5a6990679cffb22fe66c4bfc5d68
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-04-08 19:23:26 +0000

    Homogeneisation correcte.

--- src/numbers/homogen.cpp
+++ src/numbers/homogen.cpp
@@ -7,8 +7,8 @@
 struct homogen_rounding_class: rounding_class {
   interval he, hb;
   homogen_rounding_class();
-  virtual interval round                      (interval const &, std::string &) const;
-  virtual interval absolute_error_from_rounded(interval const &, std::string &) const;
+  virtual interval round                   (interval const &, std::string &) const;
+  virtual interval absolute_error_from_real(interval const &, std::string &) const;
   virtual std::string name() const { return "homogen80x"; }
 };
 
@@ -25,9 +25,30 @@ interval homogen_rounding_class::round(interval const &i, std::string &name) con
   return i * hb;
 }
 
-interval homogen_rounding_class::absolute_error_from_rounded(interval const &i, std::string &name) const {
+interval homogen_rounding_class::absolute_error_from_real(interval const &i, std::string &name) const {
   name = "homogen80x_error";
   return i * he;
 }
 
 static homogen_rounding_class dummy;
+
+struct homogen_init_rounding_class: rounding_class {
+  interval he;
+  homogen_init_rounding_class();
+  virtual interval absolute_error_from_rounded(interval const &, std::string &) const;
+  virtual std::string name() const { return "homogen80x_init"; }
+};
+
+homogen_init_rounding_class::homogen_init_rounding_class() {
+  he = from_exponent(-53, 0) + from_exponent(-64, 0);
+  ast_ident *n = ast_ident::find("homogen80x_init");
+  n->id_type = REAL_RND;
+  n->rnd = this;
+}
+
+interval homogen_init_rounding_class::absolute_error_from_rounded(interval const &i, std::string &name) const {
+  name = "homogen80x_init_error";
+  return i * he;
+}
+
+static homogen_init_rounding_class dummy_init;
