547de0a4272a4accdc7f1a0b69d31521bce818a5
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-12-21 16:45:10 +0000

    Generalisation du forcage en virgule fixe

--- src/arithmetic/fixed.cpp
+++ src/arithmetic/fixed.cpp
@@ -20,22 +20,14 @@ struct fixed_rounding_class: function_class {
   direction_type type;
   std::string ident;
   fixed_rounding_class(fixed_format const &f, direction_type t, std::string const &i)
-    : function_class(UOP_ID, TH_RND | TH_ENF | TH_ABS | (t == ROUND_ZR ? TH_ABS_REA | TH_ABS_RND : 0)),
+    : function_class(UOP_ID, TH_RND | TH_ABS | (t == ROUND_ZR ? TH_ABS_REA | TH_ABS_RND : 0)),
       format(f), type(t), ident(i) {}
   virtual interval round                      (interval const &, std::string &) const;
-  virtual interval enforce                    (interval const &, std::string &) const;
   virtual interval absolute_error                               (std::string &) const;
   virtual interval absolute_error_from_real   (interval const &, std::string &) const;
   virtual interval absolute_error_from_rounded(interval const &, std::string &) const;
   virtual std::string name() const { return "rounding_fixed" + ident; }
 };
-
-interval fixed_rounding_class::enforce(interval const &i, std::string &name) const {
-  number a = round_number(lower(i), &format, &fixed_format::roundU);
-  number b = round_number(upper(i), &format, &fixed_format::roundD);
-  name = "fixed_enforce" + ident;
-  return interval(a, (a <= b) ? b : a);
-}
 
 interval fixed_rounding_class::round(interval const &i, std::string &name) const {
   rounding_fun f = direction_functions[type];
@@ -109,7 +101,6 @@ function_class const *int_rounding_generator::operator()(function_params const &
 static int_rounding_generator dummy2;
 
 // FIX_OF_FIXED
-
 REGISTER_SCHEMEX_BEGIN(fix_of_fixed);
   long min_exp;
   fix_of_fixed_scheme(predicated_real const &r, long e)
@@ -134,7 +125,6 @@ proof_scheme *fix_of_fixed_scheme::factory(predicated_real const &real) {
 }
 
 // FIXED_OF_FIX
-
 REGISTER_SCHEME_BEGIN(fixed_of_fix);
   predicated_real fixval;
   long min_exp;
@@ -164,4 +154,33 @@ proof_scheme *fixed_of_fix_scheme::factory(ast_real const *real) {
   fixed_rounding_class const *f = dynamic_cast< fixed_rounding_class const * >(p->fun);
   if (!f) return NULL;
   return new fixed_of_fix_scheme(real, predicated_real(holders[0], PRED_FIX), f->format.min_exp);
+}
+
+// BND_OF_FIX_BND
+REGISTER_SCHEME_BEGIN(bnd_of_fix_bnd);
+  preal_vect needed;
+  bnd_of_fix_bnd_scheme(preal_vect const &v)
+    : proof_scheme(v[0]), needed(v) {}
+REGISTER_SCHEME_END(bnd_of_fix_bnd);
+
+node *bnd_of_fix_bnd_scheme::generate_proof() const {
+  property hyps[2];
+  if (!fill_hypotheses(hyps, needed)) return NULL;
+  fixed_format format(hyps[1].cst());
+  interval const &i = hyps[0].bnd();
+  number a = round_number(lower(i), &format, &fixed_format::roundU);
+  number b = round_number(upper(i), &format, &fixed_format::roundD);
+  property res(real, interval(a, (a <= b) ? b : a));
+  return create_theorem(2, hyps, res, "bnd_of_fix_bnd");
+}
+
+preal_vect bnd_of_fix_bnd_scheme::needed_reals() const {
+  return needed;
+}
+
+proof_scheme *bnd_of_fix_bnd_scheme::factory(ast_real const *real) {
+  preal_vect hyps;
+  hyps.push_back(predicated_real(real, PRED_BND));
+  hyps.push_back(predicated_real(real, PRED_FIX));
+  return new bnd_of_fix_bnd_scheme(hyps);
 }
