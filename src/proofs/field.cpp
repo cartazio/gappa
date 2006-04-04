220bce33929d04beb6f4239169b8ecf3e70b6837
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2006-04-04 17:20:03 +0000

    Un peu de nettoyage des regles

--- src/proofs/field.cpp
+++ src/proofs/field.cpp
@@ -1,3 +1,4 @@
+#include <boost/preprocessor/cat.hpp>
 #include "parser/pattern.hpp"
 
 rewriting_vect rewriting_rules;
@@ -23,153 +24,186 @@ static pattern a(0), b(1), c(2), d(3), a_b(-1), one(token_one);
 #define abs pattern::abs
 #define sqrt pattern::sqrt
 
+#define REWRITING_NAME BOOST_PP_CAT(rewriting_rule_,__LINE__)
+
 #define REWRITE(name,lhs,rhs) \
-  static rewriting_rule rewriting_rule_##name \
+  static rewriting_rule REWRITING_NAME \
   (lhs, rhs, #name, pattern_cond_vect(), pattern_excl_vect())
 #define REWRIT3(name,lhs,rhs,cond) \
-  static rewriting_rule rewriting_rule_##name \
+  static rewriting_rule REWRITING_NAME \
   (lhs, rhs, #name, pattern_cond_vect() && cond, pattern_excl_vect())
 #define REWRITe(name,lhs,rhs,excl) \
-  static rewriting_rule rewriting_rule_##name \
+  static rewriting_rule REWRITING_NAME \
   (lhs, rhs, #name, pattern_cond_vect(), pattern_excl_vect() && excl)
 #define REWRIT9(name,lhs,rhs,cond,excl) \
-  static rewriting_rule rewriting_rule_##name \
+  static rewriting_rule REWRITING_NAME \
   (lhs, rhs, #name, pattern_cond_vect() && cond, pattern_excl_vect() && excl)
 
-REWRITE(add_decomposition_rounded_left,
+/*
+Naming convention: operator name followed by
+  x (expand term)      f (factor term)      m (mix)
+  a (approximate expr) e (accurate expr)    i (irrelevant)
+  l (left hand side)   r (right hand side)  b (both)
+  s (absolute error)   q (relative error)
+*/
+
+// ADD
+
+REWRITE(add_xals,
 	b + c,
 	(b - a) + (a + c));
 
-REWRITE(add_decomposition_rounded_right,
+REWRITE(add_xars,
 	c + b,
 	(c + a) + (b - a));
 
-REWRITe(sub_decomposition_rounded_left,
-	b - c,
-	(b - a) + (a - c),
-	a ^ c);
-
-REWRITE(sub_decomposition_rounded_right,
-	c - b,
-	(c - a) + -(b - a));
-
-REWRITE(mul_decomposition_rounded_left,
-	b * c,
-	(b - a) * c + a * c);
-
-REWRITE(mul_decomposition_rounded_right,
-	c * b,
-	c * (b - a) + c * a);
-
-REWRITe(add_decomposition,
+REWRITe(add_mibs,
 	(a + b) - (c + d),
 	(a - c) + (b - d),
 	a ^ c && b ^ d);
 
-REWRITe(add_decomposition_left,
+REWRITe(add_fils,
 	(a + b) - (a + c),
 	b - c,
 	b ^ c);
 
-REWRITe(add_decomposition_right,
+REWRITe(add_firs,
 	(a + b) - (c + b),
 	a - c,
 	a ^ c);
 
-REWRITe(sub_decomposition,
+// SUB
+
+REWRITe(sub_xals,
+	b - c,
+	(b - a) + (a - c),
+	a ^ c);
+
+REWRITE(sub_xars,
+	c - b,
+	(c - a) + -(b - a));
+
+REWRITe(sub_mibs,
 	(a - b) - (c - d),
 	(a - c) + -(b - d),
 	a ^ c && b ^ d);
 
-REWRITe(sub_decomposition_left,
+REWRITe(sub_fils,
 	(a - b) - (a - c),
 	-(b - c),
 	b ^ c);
 
-REWRITe(sub_decomposition_right,
+REWRITe(sub_firs,
 	(a - b) - (c - b),
 	a - c,
 	a ^ c);
 
-REWRITe(mul_decomposition_factor_left,
+// MUL
+
+REWRITE(mul_xals,
+	b * c,
+	(b - a) * c + a * c);
+
+REWRITE(mul_xars,
+	c * b,
+	c * (b - a) + c * a);
+
+REWRITe(mul_fils,
 	a * b - a * c,
 	a * (b - c),
 	b ^ c);
 
-REWRITe(mul_decomposition_factor_right,
+REWRITe(mul_firs,
 	a * c - b * c,
 	(a - b) * c,
 	a ^ b);
 
-REWRITe(mul_decomposition_half_left,
+REWRITe(mul_mars,
 	a * b - c * d,
 	a * (b - d) + (a - c) * d,
 	a ^ c && b ^ d);
 
-REWRITe(mul_decomposition_half_right,
+REWRITe(mul_mals,
 	a * b - c * d,
 	(a - c) * b + c * (b - d),
 	a ^ c && b ^ d);
 
-REWRITe(mul_decomposition_full_left,
+REWRITe(mul_mabs,
 	a * b - c * d,
 	a * (b - d) + (a - c) * b + -((a - c) * (b - d)),
 	a ^ c && b ^ d);
 
-REWRITe(mul_decomposition_full_right,
+REWRITe(mul_mibs,
 	a * b - c * d,
 	c * (b - d) + (a - c) * d + (a - c) * (b - d),
 	a ^ c && b ^ d);
 
-REWRIT9(relative_transitivity, //relative_error_trans,
-	(b - c) / c,
-	(b - a) / a + (a - c) / c + ((b - a) / a) * ((a - c) / c),
-	~c && ~a,
-	a ^ c);
-
-REWRIT9(relative_to_absolute,
-	a - b,
-	((a - b) / b) * b,
-	~b,
-	a ^ b);
-
-REWRIT9(mul_rel_decomposition,
+REWRIT9(mul_mibq,
 	(a * b - c * d) / (c * d),
 	(a - c) / c + (b - d) / d + ((a - c) / c) * ((b - d) / d),
 	~c && ~d,
 	a ^ c && b ^ d);
 
-REWRIT9(mul_rel_decomposition_left,
+REWRIT9(mul_filq,
 	(a * b - a * c) / (a * c),
 	(b - c) / c,
 	~a && ~c,
 	b ^ c);
 
-REWRIT9(mul_rel_decomposition_right,
+REWRIT9(mul_firq,
 	(a * b - c * b) / (c * b),
 	(a - c) / c,
 	~b && ~c,
 	a ^ c);
 
-REWRIT3(square_sqrt,
-	sqrt(a) * sqrt(a),
-	a,
-	a >= 0);
+// ERR
 
-REWRITE(approx_to_accura_abs,
+REWRITe(sub_xals, //actually err_xers
+	c - a,
+	(c - b) + (b - a),
+	b ^ c);
+
+REWRIT9(err_xalq,
+	(b - c) / c,
+	(b - a) / a + (a - c) / c + ((b - a) / a) * ((a - c) / c),
+	~c && ~a,
+	a ^ c);
+
+REWRIT9(err_xalq, //actually err_xerq
+	(c - a) / a,
+	(c - b) / b + (b - a) / a + ((c - b) / b) * ((b - a) / a),
+	~a && ~b,
+	b ^ c);
+
+REWRIT9(err_xibq,
+	a - b,
+	((a - b) / b) * b,
+	~b,
+	a ^ b);
+
+// VAL
+
+REWRITE(val_xabs,
 	b,
 	a + (b - a));
 
-REWRITE(accura_to_approx_abs,
+REWRITE(val_xebs,
         a_b,
         b + -(b - a));
 
-REWRIT3(approx_to_accura_rel,
+REWRIT3(val_xabq,
 	b,
 	a * (one + (b - a) / a),
 	~a);
 
-REWRIT3(accura_to_approx_rel,
+REWRIT3(val_xebq,
 	a_b,
 	b / (one + (b - a) / a),
 	~a && ~(one + (b - a) / a));
+
+// BLI
+
+REWRIT3(square_sqrt,
+	sqrt(a) * sqrt(a),
+	a,
+	a >= 0);
