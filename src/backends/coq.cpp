f2cd6cd4664afc1f413b94dcc6f9d8880138a20b
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2007-09-14 15:36:02 +0000

    Generation des reecritures avec contraintes en Coq.

--- src/backends/coq.cpp
+++ src/backends/coq.cpp
@@ -8,6 +8,7 @@
 #include "numbers/real.hpp"
 #include "numbers/round.hpp"
 #include "parser/ast.hpp"
+#include "parser/pattern.hpp"
 #include "proofs/proof_graph.hpp"
 
 extern std::string get_real_split(number const &f, int &exp, bool &zero);
@@ -356,13 +357,102 @@ struct coq_backend: backend {
 };
 
 std::string coq_backend::rewrite(ast_real const *src, ast_real const *dst,
-                                 pattern_cond_vect const &)
+                                 pattern_cond_vect const &pc)
 {
   static int a_id = 0;
-  std::string name = composite('a', ++a_id);
+  ++a_id;
+  int nb_hyps = 0;
+  std::ostringstream s_hyps, s_intros, s_bool, s_proof, s_dec;
+  bool first_bool = true;
   auto_flush plouf;
-  plouf << "Hypothesis " << name << " : forall zi : FF, BND "
-        << display(dst) << " zi -> true = true -> BND " << display(src) << " zi.\n";
+  plouf << "Hypothesis a" << a_id << " : ";
+  for (pattern_cond_vect::const_iterator i = pc.begin(),
+       i_end = pc.end(); i != i_end; ++i)
+  {
+    std::string var = display(i->real);
+    std::string val;
+    if (i->type == COND_NZ || i->value == 0) val = "0";
+    else
+    {
+      std::ostringstream val_;
+      val_ << "Float1 (" << i->value << ')';
+      val = val_.str();
+    }
+    plouf << '(' << var;
+    switch (i->type)
+    {
+      case COND_NZ:
+      case COND_NE: plouf << " <> " << val << ")%R -> "; break;
+      case COND_LT: plouf << " < "  << val << ")%R -> "; break;
+      case COND_GT: plouf << " > "  << val << ")%R -> "; break;
+      case COND_LE: plouf << " <= " << val << ")%R -> "; break;
+      case COND_GE: plouf << " >= " << val << ")%R -> "; break;
+    }
+    if (i->type == COND_NZ)
+    {
+      s_hyps << "NZR " << var << " -> ";
+      s_intros << " h" << nb_hyps;
+      s_proof << " exact h" << nb_hyps << ".\n";
+    }
+    else
+    {
+      s_hyps << "forall i" << nb_hyps << " : FF, BND " << var << " i" << nb_hyps << " -> ";
+      s_intros << " i" << nb_hyps << " h" << nb_hyps;
+      std::string s_dec_ = s_dec.str();
+      s_dec.str(std::string());
+      if (first_bool)
+      {
+        first_bool = false;
+        s_dec << " rename hb into j" << nb_hyps << ".\n";
+      }
+      else
+      {
+        s_bool << " && ";
+        s_dec << " generalize (andb_prop _ _ hb). clear hb. intros (hb, j" << nb_hyps << ").\n";
+      }
+      if (i->value == 0)
+      {
+        char const *s = NULL;
+        switch (i->type)
+        {
+          case COND_LT: s = "lt0"; break;
+          case COND_GT: s = "gt0"; break;
+          case COND_LE: s = "le0"; break;
+          case COND_GE: s = "ge0"; break;
+          default: assert(false);
+        }
+        s_bool << "rewrite_" << s << "_helper i" << nb_hyps;
+        s_proof << " apply rewrite_" << s << " with (1 := h" << nb_hyps
+                << ") (2 := j" << nb_hyps << ").\n";
+      }
+      else
+      {
+        char const *s = NULL;
+        switch (i->type)
+        {
+          case COND_NE: s = "ne"; break;
+          case COND_LT: s = "lt"; break;
+          case COND_GT: s = "gt"; break;
+          case COND_LE: s = "le"; break;
+          case COND_GE: s = "ge"; break;
+          default: assert(false);
+        }
+        s_bool << "rewrite_" << s << "_helper i" << nb_hyps << " (" << i->value << ')';
+        s_proof << " apply rewrite_" << s << " with (1 := h" << nb_hyps
+                << ") (2 := j" << nb_hyps << ").\n";
+      }
+      s_dec << s_dec_;
+    }
+    ++nb_hyps;
+  }
+  plouf << display(src) << " = " << display(dst) << ".\n";
+  if (first_bool) s_bool << "true";
+  std::string name = composite('b', a_id);
+  plouf << "Lemma " << name << " : " << s_hyps.str() << " forall zi : FF, BND "
+        << display(dst) << " zi -> " << s_bool.str() << " = true -> BND "
+        << display(src) << " zi.\n intros" << s_intros.str()
+        << " zi hz hb.\n" << s_dec.str() << " rewrite a" << a_id
+        << ".\n exact hz.\n" << s_proof.str() << "Qed.\n";
   return name;
 }
 
