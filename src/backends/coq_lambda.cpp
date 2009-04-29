c18cb713ad7a5695213e8fc246ac1175c8e0af45
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2009-04-29 18:12:45 +0000

    Added Coq lambda-term back-end.

---
+++ src/backends/coq_lambda.cpp
@@ -0,0 +1,540 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include "backends/backend.hpp"
+#include "numbers/interval_utility.hpp"
+#include "numbers/real.hpp"
+//#include "numbers/round.hpp"
+#include "parser/ast.hpp"
+#include "proofs/proof_graph.hpp"
+#include "proofs/property.hpp"
+
+#define GAPPADEF "Gappa.Gappa_definitions."
+#define COQRDEF "Reals.Rdefinitions."
+
+static char const *theorem_defs[][2] = {
+  { "subset", "$gpred_bnd.subset $1x $1i $i $1p" },
+  { "subset_l", "$gpred_bnd.subset_l $1x $1i $l $1p" },
+  { "subset_r", "$gpred_bnd.subset_l $1x $1i $u $1p" },
+  { "abs_subset", "$gpred_abs.abs_subset $1x $1i $i $1p" },
+  { "rel_subset", "$gpred_rel.rel_subset $1x $1y $1i $i $1p" },
+
+  { "sqrt", "$gpred_bnd.sqrtG $1x $1i $i $1p" },
+
+  { "add_fix", "$gpred_fixflt.add_fix $1x $2x $1c $2c $c $1p $2p" },
+
+  { "fix_of_float", "$gfloat.fix_of_float _ _ _ _ $c" },
+
+  { "rel_of_fix_float_ne", "$gfloat.rel_of_fix_float_ne _ _ $1c $1x $i $1p" },
+
+  { NULL, NULL }
+};
+
+struct theorem_map : std::map<std::string, char const * >
+{
+  theorem_map()
+  {
+    for (char const *(*p)[2] = theorem_defs; (*p)[0]; ++p) {
+      insert(std::pair<std::string, char const *>((*p)[0], (*p)[1]));
+    }
+  }
+};
+
+theorem_map theorems;
+
+std::ostream *out_vars;
+
+extern std::string get_real_split(number const &f, int &exp, bool &zero);
+
+static std::string convert_name(std::string const &name)
+{
+  std::string::size_type p2 = name.find(',');
+  if (p2 == std::string::npos) return name;
+  std::string prefix = name.substr(0, p2);
+  bool rounding = prefix == "rounding_fixed" || prefix == "rounding_float";
+  bool fragile = false;
+  std::ostringstream res;
+  if (rounding) res << "Gappa.Gappa_" << prefix.substr(9) << '.';
+  res << prefix;
+  do {
+    std::string::size_type p1 = p2 + 1;
+    p2 = name.find(',', p1);
+    std::string s(name, p1, p2 == std::string::npos ? p2 : p2 - p1);
+    if (!std::isalpha(s[0])) {
+      res << " (" << s << ')';
+      fragile = true;
+    } else if (rounding && s.length() == 2) {
+      res << " Gappa.Gappa_round_def.round"
+          << (char)std::toupper(s[0]) << (char)std::toupper(s[1]);
+      fragile = true;
+    } else res << '_' << s;
+  } while (p2 != std::string::npos);
+  if (!fragile) return res.str();
+  return '(' + res.str() + ')';
+}
+
+static id_cache< std::string > displayed_floats;
+
+static std::string display(number const &f)
+{
+  std::ostringstream s;
+  bool zero;
+  int exp;
+  s << '(' << get_real_split(f, exp, zero);
+  s << ") (" << (zero ? 0 : exp) << ')';
+  std::string const &s_ = s.str();
+  int f_id = displayed_floats.find(s_);
+  std::string name = composite('f', f_id);
+  if (f_id >= 0)
+    *out << "let " << name << " := " GAPPADEF "Float2 " << s_ << " in\n";
+  return name;
+}
+
+static id_cache< std::string > displayed_intervals;
+
+static std::string display(interval const &i)
+{
+  std::ostringstream s;
+  s << display(lower(i)) << ' ' << display(upper(i));
+  std::string const &s_ = s.str();
+  int i_id = displayed_intervals.find(s_);
+  std::string name = composite('i', i_id);
+  if (i_id >= 0)
+    *out << "let " << name << " := " GAPPADEF "makepairF " << s_ << " in\n";
+  return name;
+}
+
+static id_cache< ast_real const * > displayed_reals;
+
+static std::string display(ast_real const *r)
+{
+  if (hidden_real const *h = boost::get< hidden_real const >(r))
+    r = h->real;
+  int r_id = displayed_reals.find(r);
+  std::string name = r->name ? '_' + r->name->name : composite('r', r_id);
+  if (r_id < 0)
+    return name;
+  if (boost::get< undefined_real const >(r)) {
+    *out_vars << '(' << name << " : " COQRDEF "R) ";
+    return name;
+  }
+  auto_flush plouf;
+  plouf << "let " << name << " := ";
+  if (ast_number const *const *nn = boost::get< ast_number const *const >(r))
+  {
+    ast_number const &n = **nn;
+    std::string m = (n.mantissa.size() > 0 && n.mantissa[0] == '+') ? n.mantissa.substr(1) : n.mantissa;
+    if (n.base == 0) plouf << "Gappa.Gappa_pred_bnd.Float1 0";
+    else if (n.exponent == 0) plouf << "Gappa.Gappa_pred_bnd.Float1 (" << m << ')';
+    else plouf << GAPPADEF "Float" << n.base << " (" << m << ") (" << n.exponent << ')';
+  }
+  else if (real_op const *o = boost::get< real_op const >(r))
+  {
+    if (o->type == ROP_FUN)
+    {
+      bool convert = o->fun->name().find("rounding") == 0;
+      if (convert) plouf << GAPPADEF "float2R (";
+      plouf << convert_name(o->fun->name()) << ' ' << display(o->ops[0]);
+      for(ast_real_vect::const_iterator i = ++(o->ops.begin()), end = o->ops.end(); i != end; ++i)
+        plouf << ' ' << display(*i);
+      if (convert) plouf << ')';
+    }
+    else
+    {
+      char const *s;
+      switch (o->type) {
+      case BOP_ADD: s = COQRDEF "Rplus"; break;
+      case BOP_SUB: s = COQRDEF "Rminus"; break;
+      case BOP_MUL: s = COQRDEF "Rmult"; break;
+      case BOP_DIV: s = COQRDEF "Rdiv"; break;
+      case UOP_ABS: s = "Reals.Rbasic_fun.Rabs"; break;
+      case UOP_SQRT: s = "Reals.R_sqrt.sqrt"; break;
+      case UOP_NEG: s = COQRDEF "Ropp"; break;
+      default: assert(false);
+      }
+      plouf << s << ' ' << display(o->ops[0]);
+      if (o->ops.size() == 2) plouf << ' ' << display(o->ops[1]);
+    }
+  }
+  else
+    assert(false);
+  plouf << " in\n";
+  return name;
+}
+
+static id_cache< std::string > displayed_properties;
+
+static std::string display(property const &p)
+{
+  std::ostringstream s;
+  predicate_type t = p.real.pred();
+  ast_real const *real = p.real.real();
+  if (p.real.pred_bnd())
+  {
+    interval const &bnd = p.bnd();
+    if (lower(bnd) == number::neg_inf) {
+      assert(t == PRED_BND);
+      s << COQRDEF "Rle " << display(real) << ' ' << display(upper(bnd));
+    } else if (upper(bnd) == number::pos_inf) {
+      assert(t == PRED_BND);
+      s << COQRDEF "Rle " << display(lower(bnd)) << ' ' << display(real);
+    }
+    else
+    {
+      switch (t) {
+      case PRED_BND:
+        s << GAPPADEF "BND " << display(real) << ' ' << display(bnd);
+        break;
+      case PRED_ABS:
+        s << GAPPADEF "ABS " << display(real) << ' ' << display(bnd);
+        break;
+      case PRED_REL:
+        s << GAPPADEF "REL " << display(real) << ' ' << display(p.real.real2())
+          << ' ' << display(bnd);
+        break;
+      default:
+        assert(false);
+      }
+    }
+  }
+  else
+  {
+    switch (t) {
+    case PRED_FIX:
+      s << GAPPADEF "FIX " << display(real) << " (" << p.cst() << ')';
+      break;
+    case PRED_FLT:
+      s << GAPPADEF "FLT " << display(real) << " (" << p.cst() << ')';
+      break;
+    case PRED_NZR:
+      s << GAPPADEF "NZR " << display(real);
+      break;
+    default:
+      assert(false);
+    }
+  }
+  std::string s_ = s.str();
+  int p_id = displayed_properties.find(s_);
+  std::string name = composite('p', p_id);
+  if (p_id >= 0)
+    *out << "let " << name << " := " << s_ << " in (* " << dump_property(p) << " *)\n";
+  return name;
+}
+
+static void apply_theorem(auto_flush &plouf, std::string const &th,
+                          property const &res, property const *hyp)
+{
+  theorem_map::const_iterator it = theorems.find(th);
+  if (it == theorems.end()) {
+    std::cerr << "Theorem '" << th
+              << "' is missing from the coq-lambda back-end. Aborting.\n";
+    exit(1);
+  }
+  char const *p = it->second;
+  for (; *p; ++p)
+  {
+    if (*p != '$') { plouf << *p; continue; }
+    ++p;
+    property const *h = &res;
+    if (*p >= '1' && *p <= '9') h = &hyp[*(p++) - '1'];
+    switch (*p) {
+      case 'g':
+        plouf << "Gappa.Gappa_";
+        break;
+      case 'i':
+        plouf << display(h->bnd());
+        break;
+      case 'l':
+        plouf << display(lower(h->bnd()));
+        break;
+      case 'u':
+        plouf << display(upper(h->bnd()));
+        break;
+      case 'c':
+        plouf << '(' << h->cst() << ')';
+        break;
+      case 'x':
+        plouf << display(h->real.real());
+        break;
+      case 'y':
+        plouf << display(h->real.real2());
+        break;
+      case 'p':
+        plouf << 'h' << h - hyp;
+        break;
+      default:
+        plouf << '$';
+    }
+  }
+}
+
+static std::string display(node *n);
+
+static std::string display(theorem_node *t)
+{
+  static int t_id = 0;
+  std::string name = composite('t', ++t_id);
+  auto_flush plouf;
+  plouf << "let " << name;
+  int num_hyp = 0;
+  for (property_vect::const_iterator i = t->hyp.begin(),
+       i_end = t->hyp.end(); i != i_end; ++i)
+  {
+    plouf << " (h" << num_hyp++ << " : " << display(*i) << ')';
+  }
+  plouf << " : " <<  display(t->res) << " := (";
+  apply_theorem(plouf, convert_name(t->name), t->res, &*t->hyp.begin());
+  plouf << " : _ -> " << display(t->res) << ") (refl_equal true) in\n";
+  return name;
+}
+
+typedef std::map< predicated_real, std::pair< int, property const * > > property_map;
+
+static void invoke_lemma(auto_flush &plouf, property_vect const &hyp, property_map const &pmap)
+{
+  for (property_vect::const_iterator j = hyp.begin(),
+       j_end = hyp.end(); j != j_end; ++j)
+  {
+    property_map::const_iterator pki = pmap.find(j->real);
+    assert(pki != pmap.end());
+    int h = pki->second.first;
+    predicate_type t = j->real.pred();
+    if (j->real.pred_bnd())
+    {
+      interval const &i = pki->second.second->bnd(), &ii = j->bnd();
+      assert(i <= ii);
+      if (ii <= i)
+        plouf << " h" << h;
+      else
+      {
+        char const *prefix = "", *suffix = "";
+        switch (t)
+        {
+          case PRED_ABS: prefix = "abs_"; break;
+          case PRED_REL: prefix = "rel_"; break;
+          case PRED_BND:
+            if (lower(ii) == number::neg_inf)
+              suffix = "_r";
+            else if (upper(ii) == number::pos_inf)
+              suffix = "_l";
+            break;
+          default: assert(false);
+        }
+        plouf << " (";
+        apply_theorem(plouf, std::string(prefix) + "subset" + suffix,
+                      *j, pki->second.second);
+        plouf << ')';
+      }
+    }
+    else if (j->real.pred_cst())
+    {
+      long c = pki->second.second->cst(), cc = j->cst();
+      assert((t == PRED_FIX && c >= cc) || (t == PRED_FLT && c <= cc));
+      if (c == cc)
+        plouf << " h" << h;
+      else
+      {
+        char const *prefix = "";
+        switch (t)
+        {
+          case PRED_FIX: prefix = "fix_"; break;
+          case PRED_FLT: prefix = "flt_"; break;
+          default: assert(false);
+        }
+        plouf << " (";
+        apply_theorem(plouf, std::string(prefix) + "subset",
+                      *j, pki->second.second);
+        plouf << ')';
+      }
+    }
+    else
+    {
+      assert(t == PRED_NZR);
+      plouf << " h" << h;
+    }
+  }
+}
+
+static void invoke_lemma(auto_flush &plouf, node *n, property_map const &pmap) {
+  if (n->type != HYPOTHESIS) {
+    plouf << display(n);
+    invoke_lemma(plouf, n->get_hypotheses(), pmap);
+  } else {
+    property_vect hyp;
+    hyp.push_back(n->get_result());
+    invoke_lemma(plouf, hyp, pmap);
+  }
+}
+
+static id_cache< node * > displayed_nodes;
+
+static std::string display(node *n)
+{
+  assert(n);
+  int n_id = displayed_nodes.find(n);
+  std::string name = composite('l', n_id);
+  if (n_id < 0) return name;
+  auto_flush plouf;
+  plouf << "let " << name;
+  property_vect const &n_hyp = n->get_hypotheses();
+  property_map pmap;
+  int num_hyp = 0;
+  for (property_vect::const_iterator i = n_hyp.begin(),
+       i_end = n_hyp.end(); i != i_end; ++i)
+  {
+    property const &p = *i;
+    plouf << " (h" << num_hyp << " : " << display(p) << ")";
+    pmap[p.real] = std::make_pair(num_hyp++, &p);
+  }
+  node_vect const &pred = n->get_subproofs();
+  if (n->type == GOAL && pred[0]->type == HYPOTHESIS) {
+    property const &p = pred[0]->get_result();
+    plouf << " (h0 : " << display(p) << ")";
+    assert(num_hyp == 0);
+    pmap.insert(std::make_pair(p.real, std::make_pair(num_hyp++, &p)));
+  }
+  property const &n_res = n->get_result();
+  std::string p_res, prefix;
+  if (n_res.null()) {
+    p_res = GAPPADEF "contradiction";
+    prefix = "absurd_";
+  } else
+    p_res = display(n_res);
+  plouf << " : " << p_res << " := (* "
+        << (!n_res.null() ? dump_property(n_res) : "contradiction") << " *)\n";
+  switch (n->type) {
+  case MODUS: {
+    for (node_vect::const_iterator i = pred.begin(),
+         i_end = pred.end(); i != i_end; ++i)
+    {
+      node *m = *i;
+      property const &res = m->get_result();
+      plouf << "  let h" << num_hyp << " : " << display(res) << " := ";
+      invoke_lemma(plouf, m, pmap);
+      plouf << " in\n";
+      pmap[res.real] = std::make_pair(num_hyp++, &res);
+    }
+    modus_node *mn = dynamic_cast< modus_node * >(n);
+    assert(mn && mn->target);
+    plouf << "  " << display(mn->target);
+    invoke_lemma(plouf, mn->target->hyp, pmap);
+    plouf << " in\n";
+    break; }
+#if 0
+  case INTERSECTION: {
+    int num[2];
+    char const *suffix = "";
+    for(int i = 0; i < 2; ++i) {
+      node *m = pred[i];
+      property const &res = m->get_result();
+      switch (res.real.pred()) {
+        case PRED_BND:
+          if (!is_bounded(res.bnd())) suffix = (i == 0) ? "_hb" : "_bh";
+          break;
+        case PRED_ABS:
+          suffix = "_aa";
+          break;
+        case PRED_REL:
+          suffix = "_rr";
+          break;
+        default:
+          assert(false);
+      }
+      if (m->type == HYPOTHESIS) {
+        property_map::iterator pki = pmap.find(res.real);
+        assert(pki != pmap.end());
+        num[i] = pki->second.first;
+        continue;
+      }
+      plouf << " assert (h" << num_hyp << " : " << display(res) << ").";
+      invoke_lemma(plouf, m, pmap);
+      num[i] = num_hyp++;
+    }
+    plouf << " apply " << prefix << "intersect" << suffix << " with"
+                 " (1 := h" << num[0] << ") (2 := h" << num[1] << ")."
+             " finalize.\nQed.\n";
+    break; }
+  case UNION: {
+    assert(pred.size() >= 2);
+    node *mcase = pred[0];
+    property const &pcase = mcase->get_result();
+    assert(pcase.real.pred() == PRED_BND);
+    property_map::mapped_type &hcase = pmap[pcase.real];
+    if (mcase->type != HYPOTHESIS) {
+      plouf << " assert (h" << num_hyp << " : " << display(pcase) << ").";
+      invoke_lemma(plouf, mcase, pmap);
+      hcase = std::make_pair(num_hyp, &pcase);
+    }
+    plouf << " generalize h" << hcase.first << ". clear h" << hcase.first << ".\n";
+    for(node_vect::const_iterator i = pred.begin() + 1, i_end = pred.end(); i != i_end; ++i) {
+      node *m = *i;
+      property_vect const &m_hyp = m->graph->get_hypotheses();
+      hcase.second = &m_hyp[0];
+      plouf << " assert (u : " << display(m_hyp[0]) << " -> " << p_res << ")."
+               " intro h" << hcase.first << ". (* " << m_hyp[0].bnd() << " *)\n";
+      property const &res = m->get_result();
+      interval const &mb = res.bnd(), &nb = n_res.bnd();
+      if (!res.null()) { // not a contradictory result
+        assert(mb <= nb);
+        if (!(nb <= mb))
+          plouf << " apply subset with " << display(mb) << ". 2: finalize.\n";
+      }
+      invoke_lemma(plouf, m, pmap);
+      if (i + 1 != i_end)
+        plouf << " next_interval (union) u.\n";
+      else
+        plouf << " exact u.\n";
+    }
+    plouf << "Qed.\n";
+    break; }
+#endif
+  case GOAL: {
+    node *m = pred[0];
+    interval const &mb = m->get_result().bnd(), &nb = n_res.bnd();
+    if (!(nb <= mb))
+    {
+      char const *suffix = "";
+      if (lower(nb) == number::neg_inf) suffix = "_r";
+      else if (upper(nb) == number::pos_inf) suffix = "_l";
+      apply_theorem(plouf, std::string("subset") + suffix,
+                    n_res, &m->get_result());
+    }
+    invoke_lemma(plouf, m, pmap);
+    plouf << " in\n";
+    break; }
+  default:
+    assert(false);
+  }
+  return name;
+}
+
+struct coq_lambda_backend: backend
+{
+  coq_lambda_backend(): backend("coq-lambda") {}
+  void initialize(std::ostream &o) { out = &o; }
+  void finalize() {}
+  void reset() { displayed_nodes.clear(); }
+  virtual std::string rewrite(ast_real const *, ast_real const *, pattern_cond_vect const &);
+  virtual std::string theorem(node *n);
+};
+
+std::string coq_lambda_backend::rewrite
+  (ast_real const *, ast_real const *, pattern_cond_vect const &)
+{
+  std::cerr << "Rewriting rules are not supported by the coq-lambda back-end.\n";
+  exit(1);
+  return "";
+}
+
+std::string coq_lambda_backend::theorem(node *n)
+{
+  out_vars = out;
+  std::ostringstream buf;
+  out = &buf;
+  *out_vars << "fun ";
+  std::string s = display(n);
+  *out_vars << "=>\n" << buf.str() << s << '\n';
+  return s;
+}
+
+static struct coq_lambda_backend dummy;
