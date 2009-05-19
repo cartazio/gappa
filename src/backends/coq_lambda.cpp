b59796a7071a3c73b408aa8006ada18588a4e284
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2009-05-19 20:16:34 +0000

    Added annotation for contradictions.

--- src/backends/coq_lambda.cpp
+++ src/backends/coq_lambda.cpp
@@ -715,7 +715,9 @@ std::string coq_lambda_backend::rewrite
 
 std::string coq_lambda_backend::theorem(node *n)
 {
-  *out << "(* " << n->get_hypotheses().size() << " *)\n(fun ";
+  *out << "(* " << n->get_hypotheses().size();
+  if (n->get_result().null()) *out << ",contradiction";
+  *out << " *)\n(fun ";
   std::ostringstream buf;
   out_vars = out;
   out = &buf;
