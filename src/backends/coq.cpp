2102c7ac30419862c37387362e6bfe3671084ecb
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2010-04-20 18:18:45 +0200

    Added a header to the source files.

--- src/backends/coq.cpp
+++ src/backends/coq.cpp
@@ -1,3 +1,14 @@
+/*
+   Copyright (C) 2004 - 2010 by Guillaume Melquiond <guillaume.melquiond@inria.fr>
+   Part of the Gappa tool http://gappa.gforge.inria.fr/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the CeCILL Free Software License Agreement or
+   under the terms of the GNU General Public License version.
+
+   See the COPYING and COPYING.GPL files for more details.
+*/
+
 #include <cctype>
 #include <iostream>
 #include <ostream>
