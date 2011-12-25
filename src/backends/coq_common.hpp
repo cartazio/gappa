17d699d6f1125b5f0d50408bde6616ec9bdd7209
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2011-12-25 11:05:54 +0100

    Start factoring code between both Coq back-ends.

---
+++ src/backends/coq_common.hpp
@@ -0,0 +1,17 @@
+/*
+   Copyright (C) 2004 - 2012 by Guillaume Melquiond <guillaume.melquiond@inria.fr>
+   Part of the Gappa tool http://gappa.gforge.inria.fr/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the CeCILL Free Software License Agreement or
+   under the terms of the GNU General Public License version.
+
+   See the COPYING and COPYING.GPL files for more details.
+*/
+
+#include <map>
+#include <string>
+
+typedef std::map<std::string, char const *> theorem_map;
+extern theorem_map theorems;
+
