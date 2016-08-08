cartazio/gappa

commit ee41c350c1bf92a8d0f55141125c6eb69f887ddd

Do not check slow convergence when handling trees.

src/proofs/schemes.cpp
@@ -555,7 +555,7 @@ static void reduce_hypotheses(node *n, std::list<logic_node *> &trees, logic_nod
 
 static bool insert_atom(logic_node *n, property const &p, int i, std::list<logic_node *> &trees, property_tree &targets, scheme_queue *pending_schemes)
 {
-  if (!top_graph->try_property(p)) return false;
+  if (!top_graph->try_property(p), false) return false;
   //std::cerr << "Creating node for " << dump_property(p) << '\n';
   node *m = new logicp_node(p, n, i);
   top_graph->insert_node(m);

src/proofs/proof_graph.cpp
@@ -428,7 +428,7 @@ int stat_successful_app = 0, stat_discarded_pred = 0, stat_intersected_pred = 0;
  *
  * @return true if the property is worth it.
  */
-bool graph_t::try_property(property const &p) const
+bool graph_t::try_property(property const &p, bool check_slow) const
 {
   assert(top_graph == this && !contradiction);
   ++stat_successful_app;
@@ -441,20 +441,19 @@ bool graph_t::try_property(property const &p) const
       property r = res;
       r.intersect(p);
       interval const &bold = res.bnd(), &bnew = r.bnd();
-      if (!is_empty(bnew))
-      {
-        if (parameter_slow_convergence >= 1) return true;
-        double f = parameter_slow_convergence,
-          dlnew = lower(bnew).to_double(), dunew = upper(bnew).to_double(),
-          dlold = lower(bold).to_double(), duold = upper(bold).to_double();
-        if (dunew - dlnew < (duold - dlold) * f) return true;
-        if (dlold < 0 && dlnew > dlold * f) return true;
-        if (dlnew > 0 && dlnew * f > dlold) return true;
-        if (dunew < 0 && dunew * f < duold) return true;
-        if (duold > 0 && dunew < duold * f) return true;
+      if (is_empty(bnew)) {
+        if (p.real.pred() != PRED_REL) return true;
+        return try_property(property(p.real.real2(), interval(0, 0)), false);
       }
-      else if (p.real.pred() != PRED_REL) return true;
-      else return try_property(property(p.real.real2(), interval(0, 0)));
+      if (!check_slow || parameter_slow_convergence >= 1) return true;
+      double f = parameter_slow_convergence,
+        dlnew = lower(bnew).to_double(), dunew = upper(bnew).to_double(),
+        dlold = lower(bold).to_double(), duold = upper(bold).to_double();
+      if (dunew - dlnew < (duold - dlold) * f) return true;
+      if (dlold < 0 && dlnew > dlold * f) return true;
+      if (dlnew > 0 && dlnew * f > dlold) return true;
+      if (dunew < 0 && dunew * f < duold) return true;
+      if (duold > 0 && dunew < duold * f) return true;
     }
     ++stat_discarded_pred;
     return false;

src/proofs/proof_graph.hpp
@@ -167,7 +167,7 @@ class graph_t
   graph_t(graph_t *, property_tree const &);
   ~graph_t();
   node *find_already_known(predicated_real const &) const;
-  bool try_property(property const &) const;
+  bool try_property(property const &, bool check_slow = true) const;
   void insert_node(node *&);
   bool try_node(node *&);
   /** Returns the hypotheses #hyps of this graph. */

commit 171e430be226a379bf479f8551329a7fefc18fa9

Fix typo introduced by commit ee41c35.

src/proofs/schemes.cpp
@@ -555,7 +555,7 @@ static void reduce_hypotheses(node *n, std::list<logic_node *> &trees, logic_nod
 
 static bool insert_atom(logic_node *n, property const &p, int i, std::list<logic_node *> &trees, property_tree &targets, scheme_queue *pending_schemes)
 {
-  if (!top_graph->try_property(p), false) return false;
+  if (!top_graph->try_property(p, false)) return false;
   //std::cerr << "Creating node for " << dump_property(p) << '\n';
   node *m = new logicp_node(p, n, i);
   top_graph->insert_node(m);